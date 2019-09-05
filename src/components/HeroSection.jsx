52030dcf2c02ddebe6abc3068ebd78e1c3df0b19
AnsonLZF <zhanfaye@gmail.com>
2019-09-05 16:28:18 +0800

    commit to test gatsby-background-image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,9 +5,19 @@ import HeroStyles from "./HeroSection.module.scss"
 
 const HeroSection = () => (
   <section className="HeroStyles.hero">
-    <h1 className="HeroStyles.title"></h1>
-    <p className="HeroStyles.subtitle"></p>
-    <Link to="/contact">Contact</Link>
+    <h1 className="HeroStyles.title">
+      The only marketing agency on{" "}
+      <span class="HeroStyles.accent-color">a mission from God</span>, fully
+      dedicated to increase your web traffic.
+    </h1>
+    <p className="HeroStyles.subtitle">
+      We’re two brothers from Chicago that are trying to pay back our debt. We
+      can help you with <strong>SEO, SEM content marketing</strong> and whatever
+      else.
+    </p>
+    <Link to="/contact" className="HeroStyles.btn-primary">
+      Contact Test
+    </Link>
   </section>
 )
 
