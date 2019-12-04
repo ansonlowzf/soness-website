b2745c3cfb8fd0d28fc6db569a0f36d866c2654c
Anson LowZF <zhanfaye@gmail.com>
2019-12-05 06:55:49 +0800

    fixing layout naming problem

--- src/pages/product/royal-jelly.jsx
+++ src/pages/product/royal-jelly.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import Layout from "../../components/Layout"
+import Layout from "../../components/layout"
 import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 
