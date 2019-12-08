fd1f33c31c7a007414293bea083375d0936177cf
AnsonLZF <zhanfaye@gmail.com>
2019-12-08 10:25:43 +0800

    remove gatsby layout.css and replace with CssBaseline by MUI

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
-
-import "./layout.css"
+import CssBaseline from "@material-ui/core"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <div
-        style={{
-          display: `grid`,
-          gridTemplateColumns: `minmax(1em, 1fr) minmax(0px, 37.5rem) minmax(1em, 1fr)`,
-        }}
-      >
-        <main style={{ gridColumn: `2/-2` }}>{children}</main>
-      </div>
+      <CssBaseline />
+      <main>{children}</main>
     </>
   )
 }
