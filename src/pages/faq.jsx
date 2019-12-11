afdde0581f661e988b300a417d1a786ea25a87f8
AnsonLZF <zhanfaye@gmail.com>
2019-12-11 11:25:56 +0800

    Add faq page

---
+++ src/pages/faq.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const FaqPage = () => {
+  return (
+    <Layout>
+      <h1>This is FAQ Page</h1>
+    </Layout>
+  )
+}
+
+export default FaqPage
