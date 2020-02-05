5ed33dd0c877f0c3d7f3edf06988b39a193ac369
Anson LowZF <zhanfaye@gmail.com>
2020-02-05 11:10:45 +0800

    fix mobile layout problem

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -49,216 +49,214 @@ const AboutUs = () => {
   return (
     <Layout>
       <SEO title="About Us" />
-      <Container maxWidth="lg">
-        <Typography
-          variant="h2"
-          component="h1"
-          align="center"
-          gutterBottom
-          className={classes.headingTopMargin}
-        >
-          {`About Us`}
-        </Typography>
-
-        <Grid container direction="column" alignItems="center">
-          <Grid container item xs={10} md={5}>
-            <Typography
-              variant="h4"
-              component="h2"
-              gutterBottom
-              className={classes.headingTopMargin}
-            >
-              What Do We Do?
-            </Typography>
-            <Typography variant="body1" component="p" gutterBottom>
-              {`Wholesale &amp; supply raw honey to resellers in Kuala Lumpur`}
-            </Typography>
-          </Grid>
-
-          <Grid
-            container
-            item
-            justify="center"
+      <Grid container direction="column" alignItems="center">
+        <Grid item xs={11}>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            gutterBottom
             className={classes.headingTopMargin}
           >
-            <Grid item xs={12} md={4}>
-              <Img fluid={data.placeholderImage1.childImageSharp.fluid} />
-            </Grid>
-          </Grid>
+            {`About Us`}
+          </Typography>
+        </Grid>
 
-          <Grid container item xs={10} md={5}>
-            <Typography
-              variant="h4"
-              component="h2"
-              gutterBottom
-              className={classes.headingTopMargin}
-            >
-              {`How Do We Get Raw Honey?`}
-            </Typography>
-            <Typography variant="body1" component="p" gutterBottom>
-              {`We travel several states, from Seremban, Melaka, Johor &amp; Bukit
-              Mertajam to purchase raw honey directly from beekeepers.`}
-            </Typography>
-          </Grid>
-
-          <Grid container item xs={10} md={5}>
-            <Typography
-              variant="h4"
-              component="h2"
-              gutterBottom
-              className={classes.headingTopMargin}
-            >
-              {`How Do We Wholesale The Honey?`}
-            </Typography>
-            <Typography variant="body1" component="p" gutterBottom>
-              {`Bee farmer sell in a bundle. Then, We filtered and packed in 10kg food grade's bottle and wholesale in Kuala Lumpur. You get real raw honey, welcome you send to lab test.`}
-            </Typography>
-          </Grid>
-
-          <iframe
+        <Grid container direction="column" item xs={11} md={3}>
+          <Typography
+            component="h2"
+            variant="h4"
+            gutterBottom
             className={classes.headingTopMargin}
+          >
+            {`What Do We Do?`}
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
+            {`Wholesale & supply raw honey to resellers in Kuala Lumpur`}
+          </Typography>
+        </Grid>
+
+        <Grid
+          container
+          item
+          justify="center"
+          className={classes.headingTopMargin}
+        >
+          <Grid item xs={11} md={3}>
+            <Img fluid={data.placeholderImage1.childImageSharp.fluid} />
+          </Grid>
+        </Grid>
+
+        <Grid container item xs={11} md={3}>
+          <Typography
+            variant="h4"
+            component="h2"
+            gutterBottom
+            className={classes.headingTopMargin}
+          >
+            {`How Do We Get Raw Honey?`}
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
+            {`We travel several states, from Seremban, Melaka, Johor & Bukit
+          Mertajam to purchase raw honey directly from beekeepers.`}
+          </Typography>
+        </Grid>
+
+        <Grid container item xs={11} md={3}>
+          <Typography
+            variant="h4"
+            component="h2"
+            gutterBottom
+            className={classes.headingTopMargin}
+          >
+            {`How Do We Wholesale The Honey?`}
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
+            {`Bee farmer sell in a bundle. Then, We filtered and packed in 10kg food grade's bottle and wholesale in Kuala Lumpur. You get real raw honey, welcome you send to lab test.`}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={5} className={classes.headingTopMargin}>
+          <iframe
             title="visiting bee farm in Johor"
-            width="560"
-            height="315"
+            height="250"
             src="https://www.youtube.com/embed/lwPCWqd58WE"
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
+        </Grid>
 
-          <Grid container item xs={10} md={5}>
-            <Typography
-              variant="h4"
-              component="h2"
-              gutterBottom
-              className={classes.headingTopMargin}
-            >
-              Why we do wholesale raw honey?
-            </Typography>
-
-            <Typography variant="body1" component="p" gutterBottom>
-              {`Of course, we need to earn money for living. Apart from that, we also gain satisfaction from doing the healthcare business. Most importantly, government officer and newspaper also reported: "It's hard to get real raw honey from the market". Raw honey is easier accessible and available in KL now.`}
-            </Typography>
-          </Grid>
-
-          <Grid
-            container
-            item
-            justify="center"
+        <Grid container item xs={10} md={3}>
+          <Typography
+            variant="h4"
+            component="h2"
+            gutterBottom
             className={classes.headingTopMargin}
           >
-            <Grid item xs={12} md={4}>
-              <Img fluid={data.placeholderImage2.childImageSharp.fluid} />
-            </Grid>
-          </Grid>
+            Why we do wholesale raw honey?
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
+            {`Of course, we need to earn money for living. Apart from that, we also gain satisfaction from doing the healthcare business. Most importantly, government officer and newspaper also reported: "It's hard to get real raw honey from the market". Raw honey is easier accessible and available in KL now.`}
+          </Typography>
+        </Grid>
 
-          <Grid container item xs={10} md={5}>
-            <Typography
-              variant="h4"
-              component="h2"
-              gutterBottom
-              className={classes.headingTopMargin}
-            >
-              {`Who buy from us`}
-            </Typography>
-            <Typography variant="body1" component="p" gutterBottom>
-              <ol>
-                <li>Regular honey drinkers who need a bigger quantity</li>
-                <li>Honey fabricator who makes honey healthcare product</li>
-                <li>
-                  Companies which want to deliver healthcare gifts to their
-                  customers
-                </li>
-                <li>Build their honey brand and business</li>
-                <li>Beauty salon which uses honey for facial program</li>
-              </ol>
-            </Typography>
-          </Grid>
-
-          <Grid
-            container
-            item
-            justify="center"
-            className={classes.headingTopMargin}
-          >
-            <Grid item xs={12} md={5}>
-              <Img fluid={data.placeholderImage3.childImageSharp.fluid} />
-            </Grid>
-          </Grid>
-          <Grid container item xs={10} md={5}>
-            <Typography
-              variant="h4"
-              component="h2"
-              gutterBottom
-              className={classes.headingTopMargin}
-            >
-              {`Conclusion`}
-            </Typography>
-
-            <Typography variant="body1" component="p" gutterBottom>
-              <ul>
-                <li>The first time, you drink honey - you feel nothing. </li>
-                <li>
-                  You drink for one month - you feel like you not easy to get
-                  tired.
-                </li>
-                <li>
-                  You drink for 1 year; people around you feel like you are
-                  different.
-                </li>
-                <li>
-                  You drink honey for 10 years; you will feel like you are 10
-                  years younger than people around.
-                </li>
-              </ul>
-            </Typography>
-            <Typography variant="h6" component="p">
-              {`It's great opportunity to start natural aging business`}
-            </Typography>
-          </Grid>
-
-          <Grid
-            container
-            item
-            direction="column"
-            justify="center"
-            alignItems="center"
-            className={classes.sectionMargin}
-          >
-            <Typography
-              variant="h4"
-              component="h2"
-              align="center"
-              gutterBottom
-              className={classes.headingTopMargin}
-            >
-              {`Start Your Honey Business Journey With Us`}
-            </Typography>
-            <Link
-              to="/contact-us"
-              style={{
-                textDecoration: `none`,
-                color: `black`,
-                fontWeight: `bolder`,
-                padding: `0.5em 1em`,
-              }}
-            >
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.headingTopMargin}
-                style={{
-                  padding: `0.75em 1.5em`,
-                  fontWeight: `bold`,
-                  letterSpacing: `1.25px`,
-                }}
-              >
-                Contact Us
-              </Button>
-            </Link>
+        <Grid
+          container
+          item
+          justify="center"
+          className={classes.headingTopMargin}
+        >
+          <Grid item xs={11} md={3}>
+            <Img fluid={data.placeholderImage2.childImageSharp.fluid} />
           </Grid>
         </Grid>
-      </Container>
+
+        <Grid container direction="column" item xs={11} md={3}>
+          <Typography
+            variant="h4"
+            component="h2"
+            gutterBottom
+            className={classes.headingTopMargin}
+          >
+            {`Who buy from us`}
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
+            <ol>
+              <li>Regular honey drinkers who need a bigger quantity</li>
+              <li>Honey fabricator who makes honey healthcare product</li>
+              <li>
+                Companies which want to deliver healthcare gifts to their
+                customers
+              </li>
+              <li>Build their honey brand and business</li>
+              <li>Beauty salon which uses honey for facial program</li>
+            </ol>
+          </Typography>
+        </Grid>
+
+        <Grid
+          container
+          item
+          justify="center"
+          className={classes.headingTopMargin}
+        >
+          <Grid item xs={11} md={3}>
+            <Img fluid={data.placeholderImage3.childImageSharp.fluid} />
+          </Grid>
+        </Grid>
+        <Grid container item xs={11} md={3}>
+          <Typography
+            variant="h4"
+            component="h2"
+            gutterBottom
+            className={classes.headingTopMargin}
+          >
+            {`Conclusion`}
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
+            <ul>
+              <li>The first time, you drink honey - you feel nothing. </li>
+              <li>
+                You drink for one month - you feel like you not easy to get
+                tired.
+              </li>
+              <li>
+                You drink for 1 year; people around you feel like you are
+                different.
+              </li>
+              <li>
+                You drink honey for 10 years; you will feel like you are 10
+                years younger than people around.
+              </li>
+            </ul>
+          </Typography>
+        </Grid>
+
+        <Grid
+          container
+          item
+          direction="column"
+          justify="center"
+          alignItems="center"
+          className={classes.sectionMargin}
+        >
+          <Typography variant="h6" component="p">
+            {`It's great opportunity to start natural aging business`}
+          </Typography>
+          <Typography
+            variant="h4"
+            component="h2"
+            align="center"
+            gutterBottom
+            className={classes.headingTopMargin}
+          >
+            {`Start Your Honey Business Journey With Us`}
+          </Typography>
+          <Link
+            to="/contact-us"
+            style={{
+              textDecoration: `none`,
+              color: `black`,
+              fontWeight: `bolder`,
+              padding: `0.5em 1em`,
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.headingTopMargin}
+              style={{
+                padding: `0.75em 1.5em`,
+                fontWeight: `bold`,
+                letterSpacing: `1.25px`,
+              }}
+            >
+              Contact Us
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+
       <Footer />
     </Layout>
   )
